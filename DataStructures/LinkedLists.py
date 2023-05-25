ff8cbb0ad786732304c7faef011997121aef3a32
Mateo Meza <mezamateoj@gmail.com>
2023-05-24 22:14:57 -0400

    Start DS and Algo learning

---
+++ DataStructures/LinkedLists.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+    def delete(self, data):
+        if self.head is None:
+            return
+
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+
+        current = self.head
+        while current.next:
+            if current.next.data == data:
+                current.next = current.next.next
+                return
+            current = current.next
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+        print()
+
+
+# Usage example:
+linked_list = LinkedList()
+linked_list.append(5)
+linked_list.append(10)
+linked_list.prepend(2)
+linked_list.delete(5)
+linked_list.display()
---
+++ DataStructures/nodes.py
@@ -0,0 +1,32 @@
+class Nodes:
+    def __init__(self, value, link_node=None):
+        self.value = value
+        self.link_node = link_node
+
+    def set_link_node(self, link_node):
+        self.link_node = link_node
+
+    def get_link_node(self):
+        return self.link_node
+
+    def get_value(self):
+        return self.value
+
+
+    
+node_1 = Nodes('I am node 1')
+node_2 = Nodes('I am node 2')
+node_3 = Nodes('I am node 3')
+
+node_1.set_link_node(node_3)
+node_3.set_link_node(node_2)
+
+# access data from nodes
+
+# from node 1 get nodes 3 data
+node_3_data = node_1.get_link_node().get_value()
+print(node_3_data)
+
+# from node 3 get nodes 2 data
+node_2_data = node_3.get_link_node().get_value()
+print(node_2_data)
